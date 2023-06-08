e9f12909788561b1b92242edf24bf06e104154b0
Dannisse Arenas <dannisse.a@gmail.com>
2023-06-08 16:48:18 -0400

    added drinks.html

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template("index.html")
-
 @app.route('/home')
 def home():
     return render_template("index.html")
@@ -17,9 +15,18 @@ def about():
 @app.route('/mainDish')
 def mainDish():
     return render_template("mainDish.html")
-
+@app.route('/adobo')
+def adobo():
+    return render_template("adobo.html")
+@app.route('/desserts')
+def desserts():
+    return render_template("desserts.html")
+@app.route('/drinks')
+def drinks():
+    return render_template("drinks.html")
 #if __name__ == '__main__':
     #app.run(debug=True)
+
 if __name__ == '__main__':
     port = int(5000)
     app.run(host='0.0.0.0', port=port, debug=True)
